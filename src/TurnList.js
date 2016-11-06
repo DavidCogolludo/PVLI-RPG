6e105d30c3a8cb3aa8863bcfb684924a13c5fbe4
David Pérez <David Prez>
2016-11-06 21:09:56 +0100

    Terminado TurnList/src (david)

--- src/TurnList.js
+++ src/TurnList.js
@@ -4,7 +4,7 @@ function TurnList() {}
 
 TurnList.prototype.reset = function (charactersById) {
   this._charactersById = charactersById;
-
+  
   this._turnIndex = -1;
   this.turnNumber = 0;
   this.activeCharacterId = null;
@@ -15,11 +15,61 @@ TurnList.prototype.next = function () {
   // Haz que calcule el siguiente turno y devuelva el resultado
   // según la especificación. Recuerda que debe saltar los personajes
   // muertos.
+    
+  this.turnNumber ++;
+  var i = 0;
+  var found = false;
+  var length = this.list.length;
+
+  while (!found){
+    i = i % length;
+    
+    var aux = this._charactersById[this.list[i]]._isDead;
+    if (!aux){
+      this.activeCharacterId = this.list[i];
+      found = true;
+    }
+    i++;
+  }
+
+  var turno = {
+    number: this.turnNumber,
+    party: this._charactersById[this.activeCharacterId].party,
+    activeCharacterId: this.activeCharacterId
+  };
+
+  return turno;
 };
 
 TurnList.prototype._sortByInitiative = function () {
   // Utiliza la función Array.sort(). ¡No te implementes tu propia
   // función de ordenación!
+  var aux = [];
+  var list = [];
+
+  for (var obj in this._charactersById){
+    aux.push({ name : obj,
+      init: this._charactersById[obj].initiative
+    })
+  }
+  //console.log(list);
+
+  aux.sort (function (a, b) {
+    if(a.init < b.init){
+      return 1;s
+    }
+    if (a.init > b.init){
+      return -1;
+    }
+    return 0;
+  });
+
+  for (var i in aux){
+    list.push(aux[i].name);
+  }
+
+  //console.log(ini);
+  return list;
 };
 
 module.exports = TurnList;
